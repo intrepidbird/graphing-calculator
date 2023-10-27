f7802e67747798c0bc7023ec5e939165e259329d
Alex Sheng <140008493+intrepidbird@users.noreply.github.com>
2023-10-27 17:11:22 -0400

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,49 @@
+// calculator.js
+$(document).ready(function() {
+    $('#calculatorForm').submit(function(e) {
+        e.preventDefault();
+        let equation = $('#equation').val();
+        let canvas = document.getElementById('graph').getContext('2d');
+        let xValues = [];
+        let yValues = [];
+
+        for (let x = -10; x <= 10; x += 0.1) {
+            xValues.push(x);
+            yValues.push(eval(equation));
+        }
+
+        let chart = new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: xValues,
+                datasets: [{
+                    label: equation,
+                    data: yValues,
+                    backgroundColor: 'rgba(0, 123, 255, 0.3)',
+                    borderColor: 'rgba(0, 123, 255, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        display: true,
+                        title: {
+                            display: true,
+                            text: 'X'
+                        }
+                    },
+                    y: {
+                        display: true,
+                        title: {
+                            display: true,
+                            text: 'Y'
+                        }
+                    }
+                }
+            }
+        });
+    });
+});
